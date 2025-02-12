97bb44bacd6c26e5483915767339728e4d802850
shrutim250 <138755641+shrutim250@users.noreply.github.com>
2025-02-12 16:54:23 +0530

    Add files via upload

---
+++ LL.java
@@ -0,0 +1,135 @@
+package LL;
+public class LL
+ {
+    private Node head;
+    private Node tail;
+    private int  size;
+
+    public LL(){
+        this.size=0;
+    }
+
+    public void insertfirst(int value){
+   Node node = new Node(value);
+   node.next=head;
+   head=node;
+   if(tail==null){
+    tail=head;
+   }
+   size+=1;
+    }
+
+    public void insertLast(int val) {
+        if (tail == null) {
+            insertfirst(val);
+            return;
+        }
+        Node node = new Node(val);
+        tail.next = node;
+        tail = node;
+        size++;
+    }
+
+
+    public void insert(int val, int index) {
+        if (index == 0) {
+            insertfirst(val);
+            return;
+        }
+        if (index == size) {
+            insertLast(val);
+            return;
+        }
+
+        Node temp = head;
+        for (int i = 1; i < index; i++) {
+            temp = temp.next;
+        }
+
+        Node node = new Node(val, temp.next);
+        temp.next = node;
+
+        size++;
+    }
+
+    public int deleteFirst() {
+        int val = head.value;
+        head = head.next;
+        if (head == null) {
+            tail = null;
+        }
+        size--;
+        return val;
+    }
+    public int deleteLast() {
+        if (size <= 1) {
+            return deleteFirst();
+        }
+
+        Node secondLast = get(size - 2);
+        int val = tail.value;
+        tail = secondLast;
+        tail.next = null;
+        size--;
+        return val;
+    }
+    public Node get(int index) {
+        Node node = head;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node;
+    }
+    public int delete(int index) {
+        if (index == 0) {
+            return deleteFirst();
+        }
+        if (index == size - 1) {
+            return deleteLast();
+        }
+
+        Node prev = get(index - 1);
+        int val = prev.next.value;
+
+        prev.next = prev.next.next;
+        size--;
+        return val;
+    }
+    public void display(){
+        Node temp =head;
+        while(temp!=null){
+            System.out.print(temp.value+"->");
+            temp=temp.next;
+        }
+        System.out.println("END");
+    }
+    private class Node {
+     private int value;
+     private Node next;
+        
+     public Node( int value){
+        this.value=value;
+     }
+     public Node( int value,Node next){
+        this.value=value;
+        this.next=next;
+     }
+
+    }
+    public static void main(String[] args) {
+        LL list=new LL();
+        list.insertfirst(4);
+        list.insertfirst(5);
+        list.insertfirst(6);
+        list.insertfirst(7);
+        list.insertLast(24);
+        list.insertLast(48);
+        list.insert(100, 3);
+      System.out.println(list.deleteFirst());
+       System.out.println(list.deleteLast());
+        System.out.println(list.delete(2));
+        list.display();
+    }
+}
+
+
